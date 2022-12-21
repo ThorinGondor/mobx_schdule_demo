a91fd886749ea5859c26cba8b773b33034b8d8a9
ThorinGondor <1360002051@qq.com>
2022-12-21 18:20:58 +0800

    init

--- src/Todo/index.js
+++ src/Todo/index.js
@@ -1,46 +1,79 @@
 import './index.css'
+import {useStore} from "../store";
+import {observer} from "mobx-react-lite";
+import {useState} from "react";
+
 function Task() {
-  return (
-    <section className="todoapp">
-      <header className="header">
-        <h1>todos</h1>
-        <input
-          className="new-todo"
-          autoFocus
-          autoComplete="off"
-          placeholder="What needs to be done?"
-        />
-      </header>
-      <section className="main">
-        <input
-          id="toggle-all"
-          className="toggle-all"
-          type="checkbox"
-        />
-        <label htmlFor="toggle-all"></label>
-        <ul className="todo-list">
-          <li
-            className="todo"
-          >
-            <div className="view">
-              <input className="toggle" type="checkbox" />
-              <label >learn react</label>
-              <button className="destroy"></button>
-            </div>
-          </li>
-          <li
-            className="todo completed"
-          >
-            <div className="view">
-              <input className="toggle" type="checkbox" defaultChecked={true}/>
-              <label >learn react</label>
-              <button className="destroy"></button>
-            </div>
-          </li>
-        </ul>
-      </section>
-    </section>
-  )
+
+    const {taskStore} = useStore()
+
+    function changeCheck(e, id) {
+        taskStore.alterData(id)
+    }
+
+    function deleteData(e, id) {
+        taskStore.deleteData(id)
+    }
+
+    function selectAll() {
+        taskStore.fullData()
+    }
+
+    function nothing() {
+
+    }
+
+    const [content, setContent] = useState("")
+
+    function recordContent(e) {
+        setContent(e.target.value)
+    }
+
+    function addData(e) {
+        if(e.keyCode === 13) {
+            taskStore.addData(content)
+            setContent("")
+        }
+    }
+
+    return (
+        <section className="todoapp">
+            <header className="header">
+                <h1>todos</h1>
+                <input
+                    className="new-todo"
+                    autoFocus
+                    autoComplete="off"
+                    placeholder="What needs to be done?"
+                    onChange={(e) => recordContent(e)}
+                    onKeyUp={(e) => addData(e)}
+                    value={content}
+                />
+            </header>
+            <section className="main">
+                <input
+                    id="toggle-all"
+                    className="toggle-all"
+                    type="checkbox"
+                />
+                <label htmlFor="toggle-all" onClick={(e) => selectAll()}></label>
+                <ul className="todo-list">
+                    {taskStore.list.map(instance => (
+                        <li className={instance.isDone ? "todo completed" : "todo"} key={instance.id}>
+                            <div className="view">
+                                {/* 单选框 */}
+                                <input className="toggle" type="checkbox" checked={instance.isDone} onChange={nothing}
+                                       onClick={(e) => changeCheck(e, instance.id)}/>
+                                <label>{instance.name}</label>
+                                <button className="destroy" onClick={(e) => deleteData(e, instance.id)}></button>
+                            </div>
+                        </li>
+                    ))}
+
+                </ul>
+            </section>
+        </section>
+    )
 }
 
-export default Task
+export default observer(Task)
--- src/store/task.Store.js
+++ src/store/task.Store.js
@@ -1,20 +1,54 @@
+import {makeAutoObservable} from 'mobx'
+import {v4 as uuid} from "uuid";
 
-import {  makeAutoObservable } from 'mobx'
 class TaskStore {
-  list = [
-    {
-      id:1,
-      name: '学习react',
-      isDone: true
-    },
-    {
-      id:2,
-      name: '搞定mobx',
-      isDone: true
+
+    fullFlag = false
+
+    list = [
+        {
+            id: 1,
+            name: '学习react',
+            isDone: true
+        },
+        {
+            id: 2,
+            name: '搞定mobx',
+            isDone: false
+        }
+    ]
+
+    constructor() {
+        makeAutoObservable(this)
     }
-  ]
-  constructor() {
-    makeAutoObservable(this)
-  }
+
+    // action 修改数据
+    alterData = (id) => {
+        const elem = this.list.find(item => (item.id === id))
+        elem.isDone = !elem.isDone
+    }
+
+    deleteData = (id) => {
+        this.list = this.list.filter(item => (item.id !== id))
+    }
+
+    fullData = () => {
+        this.list.map(instance => (instance.isDone = !this.fullFlag))
+        this.fullFlag = !this.fullFlag
+    }
+
+    addData = (content) => {
+        if (content !== null && content != "") {
+            this.list.push(
+                {
+                    id: uuid(),
+                    name: content,
+                    isDone: false
+                }
+            )
+        }
+    }
+
 }
+
 export default TaskStore
